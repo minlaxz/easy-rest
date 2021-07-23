c8511f39e93dd5197b49186801bffd705085959e
Min Latt <minminlaxz@gmail.com>
2021-07-23 12:56:22 +0630

    New route that is supposed to be protected

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
-
 const mongoose = require('mongoose');
 const cors = require('cors');
+const friendRoutes = require('./routes/friend');
+const protectedRoutes = require('./routes/protected');
 
+const app = express();
 app.use(cors({origin: 'http://localhost:3000'}));
-
 
 // parsing request of content-type as application/json
 app.use(express.json());
@@ -14,8 +14,8 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
 });
 
-const friendRoutes = require('./routes/friend');
 app.use('/api/friend', friendRoutes);
+app.use('/api/protected', protectedRoutes);
 
 const DB_CONN = 'mongodb://localhost:27017/laxzdb';
 mongoose.connect(DB_CONN,
