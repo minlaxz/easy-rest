0f995033f6696536a91657ff52c61cfc45ed70c4
Min Latt <minminlaxz@gmail.com>
2021-07-23 14:36:07 +0630

    Bearer token or authorization token implemented
    header => req.headers.authorization or bearer => req.headers.authorization.split(' ')[1]

--- routes/protected.js
+++ routes/protected.js
@@ -1,9 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
+const SECRET_KEY = 'my super duper secret';
 
 router.get('/', (req, res) => {
-    res.status(200).send('Protected is worked.');
+    res.status(200).json({ response: 'Welcome to protected endpoint.' });
 })
 
+router.post('/login', (req, res) => {
+    let nowInSec = Math.floor(Date.now() / 1000);
+    if (!req.body.username || req.body.username === "") {
+        res.status(400).json({ response: 'Username is required.' });
+    } else {
+        if (req.body.username !== 'minlaxz') {
+            res.status(403).json({ response: 'Access denied.' });
+        } else {
+            let payload = {
+                sub: req.body.username,
+                iat: nowInSec
+            }
+            let accessToken = jwt.sign(payload, SECRET_KEY, { algorithm: 'HS256' }, { expireIn: '10m' });
+            res.status(201).json({ token: accessToken });
+            console.log(accessToken)
+        }
+    }
+})
+
+const splitToken = (token) => {
+    if (token.includes("Bearer")) {
+        return token.split(" ")[1]
+    } else {
+        return token
+    }
+}
+
+const verifyToken = (req, res, next) => {
+    if (!req.headers.authorization) {
+        res.status(401).json({ response: 'Unauthorized.' });
+    } else {
+        let token = splitToken(req.headers.authorization);
+        console.log(token)
+        jwt.verify(token, SECRET_KEY, { algorithms: ['HS256'] }, (err, decoded) => {
+            if (err) {
+                res.status(401).json({ response: 'Unauthorized.' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        })
+    }
+}
+
+router.get('/sensitive', verifyToken, (req, res) => {
+    res.status(200).json({ response: `This is a sensitive resource. ${req.decoded}` });
+})
 module.exports = [router];
